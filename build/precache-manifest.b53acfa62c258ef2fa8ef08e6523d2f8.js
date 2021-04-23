2c39a9d98f41ab75262f77d05b82500a3e753b48
rishav666 <38222130+rishav666@users.noreply.github.com>
2021-04-23 10:08:37 +0530

    Add files via upload

---
+++ build/precache-manifest.b53acfa62c258ef2fa8ef08e6523d2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb88dee8b5c331356a0655199149fb3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38611ca1d5158b542d26",
+    "url": "/static/css/main.6c0ac94f.chunk.css"
+  },
+  {
+    "revision": "4dea63ec98f4a4958f92",
+    "url": "/static/js/2.02cc656a.chunk.js"
+  },
+  {
+    "revision": "38611ca1d5158b542d26",
+    "url": "/static/js/main.118d0f30.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
